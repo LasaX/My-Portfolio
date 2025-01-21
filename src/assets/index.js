04d38465913554c145c7138448dc6ab437831cd8
LasaX <155442893+LasaX@users.noreply.github.com>
2025-01-21 20:42:41 +0530

    optimize imgs

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import bannerImg from "./images/bannerImg.png";
+import bannerImg from "./images/bannerImg.webp";
 import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.png";
 import projectThree from "./images/projects/projectThree.jpeg";
